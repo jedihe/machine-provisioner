44f00c021fdf16bfa2923c43910d8f641a9f87fe
John Herreño <jedihe@gmail.com>
2015-04-19 20:27:03 -0500

    Remove old/machine-specific operations on startup.sh

--- puppet/modules/miscfiles/files/startup.sh
+++ puppet/modules/miscfiles/files/startup.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# General adjustments for jedihe's session
+# General adjustments to be done at session start
 # Use by adding to Startup Applications
 
-# Adjust gamma for SyncMaster to compensate for its darkness
-#gnome-terminal --execute `sleep 8 && xgamma -screen 0 -rgamma 1.38 -ggamma 1.40 -bgamma 1.38` &
-
-#gnome-terminal --execute `sleep 4 && disper -r 1920x1080 -e` &
 if ! pgrep redshift; then redshift -l 4.66:-74.09 -t 6500:3500 & fi
-
-# Reset left-handed button mapping for Trackball,
-# Unity/Gnome seems to override the .conf file settings for this.
-xinput set-button-map "Logitech USB Trackball" 3 2 1
-xinput set-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 1
