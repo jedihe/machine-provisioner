f894bcdd78acde8d3084cc5a48b328fe73aec77b
John Herreño <jedihe@gmail.com>
2015-04-19 20:32:52 -0500

    Restore marblemouse config for left hand use via startup.sh

--- puppet/modules/miscfiles/files/startup.sh
+++ puppet/modules/miscfiles/files/startup.sh
@@ -4,3 +4,8 @@
 # Use by adding to Startup Applications
 
 if ! pgrep redshift; then redshift -l 4.66:-74.09 -t 6500:3500 & fi
+
+# Reset left-handed button mapping for Trackball,
+# Unity/Gnome seems to override the .conf file settings for this.
+xinput set-button-map "Logitech USB Trackball" 3 2 1
+xinput set-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 1
