93056b8469bb9b7086aebecb77ba8bdbf6e309ae
John Herreño <jedihe@gmail.com>
2015-06-12 23:19:54 -0500

    Set the whole touchpad surface to perform a left click on press

--- puppet/modules/miscfiles/files/startup.sh
+++ puppet/modules/miscfiles/files/startup.sh
@@ -4,3 +4,6 @@
 # Use by adding to Startup Applications
 
 if ! pgrep redshift; then redshift -l 4.66:-74.09 -t 6500:3500 & fi
+
+# Make the whole surface perform a left click on press
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Soft Button Areas" 0, 0, 0, 0, 0, 0, 0, 0
