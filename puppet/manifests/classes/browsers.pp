0c961724d9863dcd3f81b9dfa24a05fa9cab32e1
John Herreño <jedihe@gmail.com>
2016-04-15 20:31:12 -0500

    Bye bye, flash

--- puppet/manifests/classes/browsers.pp
+++ puppet/manifests/classes/browsers.pp
@@ -1,18 +1,3 @@
 class browsers {
   package { "chromium-browser": ensure => 'installed' }
-  package { 'adobe-flashplugin': ensure => installed }
-  package { 'pepperflashplugin-nonfree': ensure => installed }
-
-  ppa_package { "pepper flash":
-    ppa => 'ppa:skunk/pepper-flash',
-    package => 'pepflashplugin-installer',
-  }
-
-  $flash_confline = '". /usr/lib/pepflashplugin-installer/pepflashplayer.sh"'
-  exec { 'enable pepper flash on Chromium':
-    command => "echo $flash_confline >> /etc/chromium-browser/default",
-    unless => 'grep "pepflashplugin" /etc/chromium-browser/default',
-    require => Ppa_package['pepper flash'],
-    path => ['/bin'],
-  }
 }
