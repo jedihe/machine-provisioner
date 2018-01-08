16b553c5813e7d6cc2d697199282875608342746
John Herreño <jedihe@gmail.com>
2018-01-08 01:16:40 -0500

    Update to oracle-java8-installer

--- puppet/manifests/classes/oracle_java.pp
+++ puppet/manifests/classes/oracle_java.pp
@@ -1,19 +1,19 @@
 class oracle_java {
   apt::ppa { 'ppa:webupd8team/java': }
 
-  exec { 'accept-java7-license':
+  exec { 'accept-java8-license':
     command => "echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections &&
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections",
     logoutput => true,
     timeout => 10,
     path => ['/bin/', '/usr/bin/'],
-    unless => "debconf-get-selections | grep oracle-java7-installer",
+    unless => "debconf-get-selections | grep oracle-java8-installer",
     require => [Apt::Ppa['ppa:webupd8team/java']],
   }
 
   # This doesn't work because of an interactive license screen
-  package { 'oracle-java7-installer':
+  package { 'oracle-java8-installer':
     ensure => 'installed',
-    require => [Class['Apt::Update'], Exec['accept-java7-license']],
+    require => [Class['Apt::Update'], Exec['accept-java8-license']],
   }
 }
