8c8bbae853271f51d6faae69bf6c9f6f5c7c3df3
John Herreño <jedihe@gmail.com>
2018-01-08 00:31:43 -0500

    Disable hibernate file from 14.04 (temporarily?)

--- puppet/manifests/classes/hibernation.pp
+++ puppet/manifests/classes/hibernation.pp
@@ -1,11 +1,11 @@
 class hibernation {
-  file { "hibernate polkit":
-    ensure => present,
-    source => "puppet:///modules/miscfiles/com.ubuntu.enable-hibernate.pkla",
-    path => "/var/lib/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla",
-    mode => 644,
-    owner => "root",
-    group => "root",
-    replace => true,
-  }
+# file { "hibernate polkit":
+#   ensure => present,
+#   source => "puppet:///modules/miscfiles/com.ubuntu.enable-hibernate.pkla",
+#   path => "/var/lib/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla",
+#   mode => 644,
+#   owner => "root",
+#   group => "root",
+#   replace => true,
+# }
 }
